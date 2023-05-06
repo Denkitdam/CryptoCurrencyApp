dca9d18a8af35ebfded61548777b67d2c156a350
denkitdam <cryptoden145@gmail.com>
2023-05-06 23:58:41 +0300

    Model created

---
+++ CryptoCurrencyApp/Models/CryptoCurrency.swift
@@ -0,0 +1,26 @@
+//
+//  CryptoCurrency.swift
+//  CryptoCurrencyApp
+//
+//  Created by Denis Kitaev on 06.05.2023.
+//
+
+import Foundation
+
+struct CryptoCurrency {
+    let id: String
+    let icon: URL
+    let name: String
+    let symbol: String
+    let rank: String
+    let price: Double
+    let priceBtc: Double
+    let volume: Double
+    let marketCap: Double
+    let availableSupply: Double
+    let totalSupply: Double
+    let priceChange1h: Double
+    let priceChange1d: Double
+    let priceChange1w: Double
+    
+}
