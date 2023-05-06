60ce31c08ae76c8e2cc8c83138be5192ba129d04
denkitdam <cryptoden145@gmail.com>
2023-05-07 00:01:32 +0300

    Created class for table view

---
+++ CryptoCurrencyApp/ViewControllers/CurrenciesListViewController.swift
@@ -0,0 +1,35 @@
+//
+//  CurrenciesListViewController.swift
+//  CryptoCurrencyApp
+//
+//  Created by Denis Kitaev on 06.05.2023.
+//
+
+import UIKit
+
+class CurrenciesListViewController: UITableViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+
+}
