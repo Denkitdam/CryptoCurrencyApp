5a3f95fe8f589941748253d3a1e4fe8ed65745f7
denkitdam <cryptoden145@gmail.com>
2023-05-10 17:24:03 +0300

    Network manager setup

---
+++ CryptoCurrencyApp/Servises/NetworkManager.swift
@@ -0,0 +1,49 @@
+//
+//  NetworkManager.swift
+//  CryptoCurrencyApp
+//
+//  Created by Denis Kitaev on 10.05.2023.
+//
+
+import Foundation
+
+enum NetworkError: Error {
+    case noData
+    case decodingError
+}
+
+class NetworkManager {
+    static let shared = NetworkManager()
+    
+    private init() {}
+    
+    func fetchCoins(with url: URL, completion: @escaping(Result<CoinList, NetworkError>) -> Void) {
+        URLSession.shared.dataTask(with: url) { data, _, error in
+            guard let data else {
+                completion(.failure(.noData))
+                print(error?.localizedDescription ?? "No Localised description")
+                return
+            }
+            do {
+                let decoder = JSONDecoder()
+                let coinList = try decoder.decode(CoinList.self, from: data)
+                completion(.success(coinList))
+            } catch {
+                completion(.failure(.decodingError))
+                print(error)
+            }
+        }.resume()
+    }
+    func fetchLogo(with url: URL, completion: @escaping(Result<Data, NetworkError>) -> Void) {
+        DispatchQueue.global().async {
+            guard let imageData = try? Data(contentsOf: url) else {
+                completion(.failure(.noData))
+                return
+            }
+            DispatchQueue.main.async {
+                completion(.success(imageData))
+            }
+                    
+        }
+    }
+}
